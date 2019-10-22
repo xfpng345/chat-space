5ed2eef055c7716319bfc2c07684e166300751a1
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-10-22 22:10:51 +0900

    groupコントローラのeditアクションからビューを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 root 'messages#index'
 resources :messages, only: [:index]
 resources :users, only: [:update, :edit]
+resources :groups, only: [:new, :create, :edit, :update]
 end
