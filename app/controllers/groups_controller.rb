69c0b7e877a60605a4c9fec8ed5fe95ef18ddc9a
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-10-25 14:55:28 +0900

    Update groups_controller.rb
    editアクションの省略

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
+  before_action :set_group, only: [:edit, :update,]
   def index
   end
 
@@ -23,9 +23,6 @@ class GroupsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def edit
   end
 
   private
