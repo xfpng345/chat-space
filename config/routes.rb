2d1ee38b813b7a4b7bd12b250c7fcf0e6d92e7b3
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-10-25 14:56:29 +0900

    Update routes.rb
    ルートパスの変更とネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 root 'groups#index'
-resources :messages, only: [:index]
 resources :users, only: [:update, :edit]
-resources :groups, only: [:new, :create, :edit, :update, :index]
+resources :groups, only: [:new, :create, :edit, :update, :index] do
+  resources :messages, only: [:index]
+end
 end
