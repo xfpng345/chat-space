b65d96cba18f464448c5b1d39c6fd561cb1ee502
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-10-31 17:30:02 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,14 @@
 $(function(){
   $("#new_message").on('submit', function(e){
-    console.log("OK");
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
   })
 });
