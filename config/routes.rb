767ce2ec93bfeebaa4689767a1ff90dbd03e78af
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-11-14 18:14:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 root 'groups#index'
-resources :users, only: [:update, :edit]
+resources :users, only: [:update, :edit,:index]
 resources :groups, only: [:new, :create, :edit, :update, :index] do
   resources :messages, only: [:index, :create,]
 end
