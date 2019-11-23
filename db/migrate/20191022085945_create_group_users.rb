9e8627fdd3d28aecda51b37a4e0499e9ee6864bf
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-11-23 19:12:11 +0900

    Update 20191022085945_create_group_users.rb

--- db/migrate/20191022085945_create_group_users.rb
+++ db/migrate/20191022085945_create_group_users.rb
@@ -1,4 +1,4 @@
-jclass CreateGroupUsers < ActiveRecord::Migration[5.0]
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.references :group, foreign_key: true
